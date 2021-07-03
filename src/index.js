4b2a22d2dc39f2a2886f17dfe539f6e53638fe3c
Andrey Tychinskiy <andreytichinsky@gmail.com>
2021-07-03 20:09:47 +0300

    chore: change log to error

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ const App = () => {
   });
 
   posts.addEventListener("click", (event) => {
-    console.log(event.path);
+    console.error(event.path);
     const liHash = event.path.reduce((acc, node) => {
       if (acc === null) {
         const hash = $(node).data("hash");
