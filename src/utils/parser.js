d88b78236b4d5ca1c9a4cadb41f5950c2f22bec0
Andrey Tychinskiy <andreytichinsky@gmail.com>
2021-07-03 14:38:44 +0300

    chore: finish step-5

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,15 +1,21 @@
 const parser = (html) => {
   const title = html.querySelector("title").textContent;
   const description = html.querySelector("description").textContent;
+  const mainLink = html.querySelector("link").textContent;
   const itemsArr = Array.from(html.querySelectorAll("item"));
   const items = {};
+  const domain = new URL(mainLink);
+  const nameForHash = domain.hostname.replace("www.", "");
   itemsArr.forEach((item) => {
     const pubDate = item.querySelector('pubDate').textContent;
     const formattedDate = Date.parse(pubDate);
-    items[formattedDate] = {
+    const link = item.querySelector('link').textContent;
+    const hash = nameForHash + formattedDate;
+    items[hash] = {
       title: item.querySelector('title').textContent,
       description: item.querySelector('description').textContent,
-      link: item.querySelector('link').textContent,
+      link,
+      hash
     };
   });
   return {
